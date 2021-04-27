824bd319c8f4c3378bb6077bfd7b240e28414846
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-04-27 20:54:02 +0530

    quiabrin

--- lib/quizbrain.dart
+++ lib/quizbrain.dart
@@ -1,7 +1,9 @@
 import 'question.dart';
 
 class QuizBrain {
-  List<Question> questionBnak = [
+  int _qno = 0;
+
+  List<Question> _questionBnak = [
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
@@ -28,4 +30,18 @@ class QuizBrain {
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
   ];
+
+  void nextQ() {
+    if (_qno < _questionBnak.length - 1) {
+      _qno++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBnak[_qno].questionTxt;
+  }
+
+  bool getAnswer() {
+    return _questionBnak[_qno].answer;
+  }
 }
